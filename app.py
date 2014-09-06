caa58fa4aa1a842badf88e95de46dee0437eb927
mengsquared <mengmengjie@0587359311.wireless.umich.net>
2014-09-06 13:58:15 -0400

    pass text to DB

--- app.py
+++ app.py
@@ -1,9 +1,50 @@
-from flask import Flask
+from flask import Flask, request, redirect
+from twilio.rest import TwilioRestClient
+import twilio.twiml
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://sentencesoup.firebaseio.com', None)
+
+account_sid = "ACec64a5b0feb7121fd6a33718d1fc4390"
+auth_token = "0a4ecac03a0cb832a0cbfd221c329b20"
+client = TwilioRestClient(account_sid, auth_token)
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+DB = 'lib/db_methods.py'
+
+# @app.route("/")
+# def index():
+#     return "Hello World!"
+
+@app.route("/", methods=['GET', 'POST'])
+def getResponse():
+	word = request.values.get('Body', None)
+
+	resp = twilio.twiml.Response()
+	resp.message(word)
+
+	result = firebase.post('/texts/', {'Sentence': word})
+
+	return str(resp)
 
 if __name__ == "__main__":
     app.run()
+
+
+
+# from flask import Flask, request, session, g, redirect, url_for, abort, \
+#     render_template, flash, _app_ctx_stack
+# from flask.ext.triangle import Triangle
+
+# app = Flask(__name__, static_path='/static')
+# Triangle(app)
+
+# @app.route("/")
+# def index():
+#   return_vals = {
+#     'title': 'Sentence Soup'
+#   }
+#   return render_template('index.html', return_vals=return_vals)
+
+# if __name__ == "__main__":
+#     app.run()
--- lib/sentence_gen.py
+++ lib/sentence_gen.py
@@ -1,7 +1,61 @@
 # Generate random sentences given input
-
 import sys, urllib3
 import numpy as np
+import simplejson as sjson
+import threading
+import requests
+import json
+import sys
+import curses
+from firebase import firebase
+
+# from twilio.rest import TwilioRestClient
+
+# from flask import Flask, request, redirect
+# import twilio.twiml
+# app = Flask(__name__)
+
+# from django_twilio.decorators import twilio_view
+# from twilio.twiml import Response
+
+
+# from twilio.rest import TwilioRestClient
+
+# account_sid = "ACec64a5b0feb7121fd6a33718d1fc4390"
+# auth_token = "0a4ecac03a0cb832a0cbfd221c329b20"
+# client = TwilioRestClient(account_sid, auth_token)
+
+firebase = firebase.FirebaseApplication('https://sentencesoup.firebaseio.com', None)
+
+# message = client.messages.create(body="Jenny please?! I love you <3",
+#     to="+12488800325",    # Replace with your phone number
+#     from_="+19513833688") # Replace with your Twilio number
+# print message.sid
+
+# @app.route("/", methods=['GET', 'POST'])
+# def hello_monkey():
+#     """Respond to incoming calls with a simple text message."""
+ 
+#     resp = twilio.twiml.Response()
+#     resp.message("Hello, Mobile Monkey")
+#     return str(resp)
+
+# for message in client.messages.list():
+#     print message.body
+
+# @twilio_view
+# def sms(request):
+#     name = request.POST.get('Body', '')
+#     msg = 'Hey %s, how are you today?' % (name)
+#     r = Response()
+#     r.message(msg)
+#     return r
+
+# name = client.request('Body', '')
+# print name
+
+
+
 
 def getRandomText(text_lists):
   rn = np.random.random_integers(0, len(text_lists)-1)
@@ -45,4 +99,10 @@ texts = [
 ]
 for i in range(0, num_sentences):
   r = http.request('GET', getRandomText(texts))
-  print getSentence(r.data) + "\n\n"
+  thisSent = getSentence(r.data);
+  print thisSent + "\n\n"
+  result = firebase.get('/', 'Mengmeng')
+  print result
+  result = firebase.post('/texts/', {'Sentence': thisSent})
+  print result
+
