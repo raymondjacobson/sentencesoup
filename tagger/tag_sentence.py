292ab6796dbbdf1c06f9ff6c489e69f68fd1274e
Maureen Daum <daummaureen@gmail.com>
2014-09-06 10:39:02 -0400

    no more sample input

--- tagger/tag_sentence.py
+++ tagger/tag_sentence.py
@@ -19,20 +19,35 @@ def getTaggedString(sentence):
 
 	regex_match = re.findall(r'(\S+)/(\S+)', tagged_string)
 	if regex_match:
-		process(regex_match)
-	return 0
+		words_and_types = process(regex_match)
+
+		# check result
+		for item in words_and_types.keys():
+			print item
+			print words_and_types[item]
+
+	return words_and_types
 
 def process(matched):
+	words_and_types = {}
 	for group in matched:
 		word = group[0]
 		tag = group[1]
-		print word + ", " + tag
+		
+		# check if words_and_types contains tag
+		# if yes: add word to array
+		# if no: create new key and array
+		if (not(words_and_types.has_key(tag))):
+			words_and_types[tag] = []		
+		words_and_types[tag].append(word)
+
+	return words_and_types
 
 
 def tagSentenceStructure(sentence_file_name):
 	f = open(r'output.txt', 'w')
 	subprocess.call(["javac", "-cp", "stanford-postagger.jar", "TaggerDemo.java"])
-	subprocess.call(["java", "-cp", """.:stanford-postagger.jar""", "TaggerDemo", "models/english-left3words-distsim.tagger", "sample-input.txt"], stdout = f)
+	subprocess.call(["java", "-cp", """.:stanford-postagger.jar""", "TaggerDemo", "models/english-left3words-distsim.tagger", sentence_file_name], stdout = f)
 	f.close()
 	return 'output.txt'
 	# print java_output.stdout
